e32080824e9b22aa208e80bdccd71788fa4a5abf
manulopez96 <d.manu.lopez@gmail.com>
2024-01-12 21:44:45 -0300

    agregando links

---
+++ portafolio/relojDigital/js/app.js
@@ -0,0 +1,28 @@
+const mostrarReloj = () => {
+    let fecha = new Date();
+    let hora = formatoHora(fecha.getHours());
+    let minutos = formatoHora(fecha.getMinutes());
+    let segundos = formatoHora(fecha.getSeconds());
+    document.getElementById('hora').innerHTML = `${hora}:${minutos}:${segundos}`
+
+    const meses = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+    const dias = ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab', 'Dom'];
+    let diaSemana = dias[fecha.getDay()];
+    let dia = fecha.getDate();
+    let mes = meses[fecha.getMonth()];
+    let año = fecha.getFullYear();
+    let fechaTexto = `${diaSemana}, ${dia} ${mes} ${año}`;
+    console.log(`fecha: ${fechaTexto}`);
+    document.getElementById("fecha").innerHTML = fechaTexto; 
+    
+    document.getElementById("contenedor").classList.toggle('animar'); 
+}
+
+const formatoHora = (hora) => {
+    if(hora < 10){
+        hora = `0${hora}`;
+    }
+    return hora;
+}
+
+setInterval(mostrarReloj, 1000);
