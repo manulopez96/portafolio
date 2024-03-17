1142f25fc7023bc3f849d5da98fc0ce05bb41f67
manulopez96 <d.manu.lopez@gmail.com>
2024-03-17 04:00:49 -0300

    mejora visual portafolio, preparado para agregar links de redireccionado, se prepara div en contacto para agregar formulario

--- src/component/Portafolio.js
+++ src/component/Portafolio.js
@@ -8,30 +8,35 @@ export const Portafolio = () => {
             url: "/img/proyectos/proyecto01.png",
             descripcion:
                 "Aplicacion que permite introducir personas a una lista, armado un array de objetos para mostrarlo dinamicamente en la pantalla.",
+            link: "#",
         },
         {
             nombre: "Reloj digital",
             url: "/img/proyectos/proyecto02.png",
             descripcion:
                 "Proyecto sencillo en Javascript, con manejo del DOM y utlizacion de eventos.",
+            link: "#",
         },
         {
             nombre: "Gestion de presupuesto",
             url: "/img/proyectos/proyecto03.png",
             descripcion:
-                "Aplicacion que permite introducir ingresos y egresos de capital, permitiendonos obtener un balance final con los datos introducidos, desarrollado con JS, html y css."
+                "Aplicacion que permite introducir ingresos y egresos de capital, permitiendonos obtener un balance final con los datos introducidos, desarrollado con JS, html y css.",
+            link: "#",
         },
         {
             nombre: "Numero aleatorio",
             url: "/img/proyectos/proyecto04.png",
             descripcion:
                 "Proyecto que permite generar un numero aleatorio, introduciendo el valor minimo y maximo, desarrollado con JS, html y css. ",
+            link: "#",
         },
         {
             nombre: "LPZ web",
             url: "/img/proyectos/proyecto05.png",
             descripcion:
                 "Proyecto personal, esta pagina, desarrollado con reactJS, utlizando hooks, props, eventos , funciones callback y mucho mas!",
+            link: "#",
         },
     ];
     const modificarContador = (op = "+") => {
@@ -51,14 +56,14 @@ export const Portafolio = () => {
         //  console.log(contador);
     };
 
-    const handleKeyDown = e => {
-        if(e.keyCode === 37){
+    const handleKeyDown = (e) => {
+        if (e.keyCode === 37) {
             modificarContador("-");
         }
-        if(e.keyCode === 39){
+        if (e.keyCode === 39) {
             modificarContador("+");
         }
-    }
+    };
 
     useEffect(() => {}, [contador]);
     return (
@@ -66,8 +71,22 @@ export const Portafolio = () => {
             <h1>Proyectos</h1>
             <br></br>
             <div className="contenedor-slider">
-                <button onClick={() => modificarContador("-")} onKeyDown={handleKeyDown} tabIndex={0} className="btn-izq">{"<"}</button>
-                <button onClick={() => modificarContador("+")} onKeyDown={handleKeyDown} tabIndex={0} className="btn-der">{">"}</button>
+                <button
+                    onClick={() => modificarContador("-")}
+                    onKeyDown={handleKeyDown}
+                    tabIndex={0}
+                    className="btn-izq"
+                >
+                    {"<"}
+                </button>
+                <button
+                    onClick={() => modificarContador("+")}
+                    onKeyDown={handleKeyDown}
+                    tabIndex={0}
+                    className="btn-der"
+                >
+                    {">"}
+                </button>
                 <div className="contenedor-img">
                     <img alt="" src={proyectos[contador].url}></img>
                     <p className="nombre-proyecto">
@@ -79,6 +98,5 @@ export const Portafolio = () => {
                 </div>
             </div>
         </div>
-
     );
 };
