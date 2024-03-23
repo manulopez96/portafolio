6144ae079b61d628af6cf8e1413d0b86fae0febd
manulopez96 <d.manu.lopez@gmail.com>
2024-03-23 19:32:41 -0300

    Se agrega formulario y api de envio

--- src/component/Contacto.js
+++ src/component/Contacto.js
@@ -1,8 +1,34 @@
 import React from "react";
 
 export const Contacto = () => {
+    let url = "http://localhost:3020/send-email";
+
+    const enviarForm = async (e) => {
+        e.preventDefault();
+        const data = {
+            name: e.target.nombre.value,
+            email: e.target.contacto.value,
+            message: `${e.target.asunto.value}\n${e.target.contenido.value}`,
+        };
+        // console.log(data);
+        if (e.target.contenido.value !== "") {
+            try {
+                await fetch(url, {
+                    method: "POST",
+                    body: JSON.stringify(data),
+                    headers: { "Content-Type": "application/json" },
+                });
+                alert("Correo enviado correctamente");
+            } catch (error) {
+                alert("Error al enviar correo");
+            }
+        } else {
+            alert("No olvide agregar contenido ;)");
+        }
+    };
+
     return (
-        <div className="page">
+        <div className="page contact">
             <div className="contacto">
                 <a href="mailto:d.manu.lopez@gmail.com" rel="noreferrer">
                     <div className="contenedor-logo">
@@ -72,6 +98,32 @@ export const Contacto = () => {
                         <p>¿Quieres escribirme por whatsapp?</p>
                     </div>
                 </a>
+            </div>
+            <div className="formulario">
+                <form onSubmit={enviarForm}>
+                    <h3>Puedes escribirme</h3>
+                    <input
+                        type="text"
+                        name="nombre"
+                        placeholder="Nombre"
+                    ></input>
+                    <input
+                        type="email"
+                        name="contacto"
+                        placeholder="Contacto"
+                    ></input>
+                    <input
+                        type="text"
+                        name="asunto"
+                        placeholder="Asunto"
+                    ></input>
+                    <textarea
+                        type="text"
+                        name="contenido"
+                        placeholder="Contenido"
+                    ></textarea>
+                    <input type="submit" value="Enviar" placeholder=""></input>
+                </form>
             </div>
         </div>
     );
